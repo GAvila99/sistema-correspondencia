178336717965f5d5be8b1b4b02e6f756da021e87
jac0230023 <jac0230023@aluno.firjansenaisesi.com.br>
2025-07-11 20:53:19 +0000

    import classe sistemaNotificacao

---
+++ scr/main/java/com/gavila/correspondencia/SistemaNotificacao.java
@@ -0,0 +1,12 @@
+  package com.gavila.correspondencia;
+
+  import com.edifice.correspondencia.model.Apartamento;
+  import com.edifice.correspondencia.model.Correspondencia;
+
+  import java.time.LocalDate; // Para trabalhar com datas de forma mais robusta
+  import java.time.format.DateTimeFormatter; // Para formatar a data
+  import java.util.HashMap;    // Importa a classe HashMap
+  import java.util.Map;        // Importa a interface Map
+  import java.util.Scanner;    // Para ler a entrada do usuário
+
+public class SistemaNotificacao
