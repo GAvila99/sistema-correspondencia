595cb81cbda73cd64a10a2aad1f5bdb5da2e533d
jac0230023 <jac0230023@aluno.firjansenaisesi.com.br>
2025-07-15 20:56:58 +0000

    criação do metodo listarcorrespondencias, registrar e adicionar apto

--- scr/main/java/com/gavila/correspondencia/SistemaNotificacao.java
+++ scr/main/java/com/gavila/correspondencia/SistemaNotificacao.java
@@ -9,4 +9,54 @@
   import java.util.Map;        // Importa a interface Map
   import java.util.Scanner;    // Para ler a entrada do usuário
 
-public class SistemaNotificacao
+public class SistemaNotificacao{
+  //map rastreia chaves para valores
+  private Map<Integer, Apartamento> apartamentos;
+
+    //contrutor da classe  SistemaNotificacao
+  public SistemaNotificacao(){
+    this.apartamento = new HashMap<>();
+  }
+
+  public Apartamento adicionarApartamento(int numeroApartamento){
+    //se nao existir cria e adiciona
+    partamentos.putIfAbsent(numeroApartamento, new Apartamento(numeroApartamento));
+    //retorna o ap novo ou ja existente
+    return apartamentos.get(numeroApartamento)
+  }
+
+  //Registra uma noca correspondencia para um ap
+  public void resgistrarCorrespondencia(int numeroApartamento, String tipo, String remetente){
+    //obtem o apartamento, cria se nao exeistir
+    Apartamento apto = adicionarApartamento(numeroApartamento);
+    
+    //obtem a data atual do sistema no dd/mm/yyyy
+    String dataAtual = LocalDate.now().formate(DateTimeFormatter.ofPattern("dd/MM/yyyy"));//localdate.now obtem a data atual, depois .format() utiliza o datetimeformatter para configurar no padrao informado.
+    
+    Correspondencia novaCorrespondencia = new Correspondencia(tipo, remetente, dataAtual);
+
+    //adiciona correspondencia e imprime uma mensagem confirmação
+    apto.adicionarCorrespondencia(novaCorrespondencia);
+    System.out.println("Correspondencia ('" + tipo + "' de '" + remetente + "') registrada para o Apt. " + numeroApartamento + ".");
+  }
+
+  //lista todas as correspondencias recebida por um apartamento
+  public void listarCorrespondenciasApartamento(int numeroApartamento){
+    if (apartamentos.containsKey(numeroApartamento)){ //verifica se o apto existe
+      Apartamento apto == apartamentos.get(numeroApartamento);
+      List<Correspondencia> correspondencias = apto.getCorrespondencias();
+
+      System.out.println("\n--- Correspondencias para o Apartamento " + numeroApartamento + " ---");
+      if (correspondencias.isEmpty()){
+        System.out.println("Nenhuma correspondencia registrada para este apartamento.");
+      }else{
+        for(correspondencia c : correspondencias){
+          System.out.println("- " + c);
+        }
+      }
+      System.out.println("----------------------------------------------");
+    }else{
+      System.out.println("Apartamento " + numeroApartamento + " não encontrado no sistema.");
+    }
+  }
+}
